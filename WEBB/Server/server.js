10f6d51046f03765034fdd1480e3981cc09e3466
PikkuHurja <pikku.hurja@gmail.com>
2023-06-02 15:21:11 +0300

    fixed shenanigans

--- WEBB/Server/server.js
+++ WEBB/Server/server.js
@@ -1,3 +1,10 @@
+
+/**
+ * @type  {socket.Server}
+ */
+let server_socket;
+
+
 const express = require("express")
 const server = express()
 const https = require("https")
@@ -5,9 +12,75 @@ const fs = require("fs")
 const path = require("path")
 const j = path.join
 
+const s_l = require("./sock_log")
+
 const socket = require("socket.io")
 
 const bParse = require("body-parser")
+const base_socket = "/socket/"
+
+/**
+ * @type {[newGame]}
+ */
+let games = {}
+
+
+class newGame{
+    /**
+     * 
+     * @param {*} req 
+     * @param {socket.Server} socket 
+     */
+    constructor(req, socket){
+        this.status = 0
+        if(req.body == undefined){
+            this.status = "No Body"
+            return 0
+        }
+        if(req.body.hostname == undefined){
+            this.status = "No Hostname"
+            return 0
+        }
+
+        this.hostname = req.body.hostname
+        this.password = req.body.password
+        this.connecions = []
+
+        console.log("Listeing..", j(base_socket, this.hostname))
+        this.socket_namespace = socket.of(j(base_socket, this.hostname))
+    }
+    /**
+     * 
+     * @param {socket.Socket} incoming_socket 
+     */
+    acceptConnection(incoming_socket){
+        console.log("gea")
+        console.log(incoming_socket.socket_info)
+        if(incoming_socket.socket_info.password == this.password){
+            if(this.connecions.length == 0){
+                incoming_socket.socket_info.host = true
+            }
+            this.connecions.push(incoming_socket)
+            incoming_socket.emit("success", (true))
+            incoming_socket.on("start", this.startGame)
+        }
+        else{
+            console.log("destroying socket")
+            incoming_socket.disconnect()
+        }
+    }
+    startGame(something){
+        console.log("startGame", something)
+    }
+    disconnectSocket(socket){
+        const index = connecions.indexOf(socket);
+        console.log("disconnected", socket.socket_info)
+        if (index > -1) { 
+            connecions.splice(index, 1); // 1 is for only 1
+        }
+    }
+
+}
 
 
 /*
@@ -16,42 +89,46 @@ const thread = require("worker_threads")
 const socket_worker = new thread.Worker(j(__dirname, "socket_worker.js"))
 */
 
-function newSocketHandle(socket){
-    console.log(socket)
+
+
+function newJoin(socket){
 
     socket.on("join", (json) => {
+        console.log("newJoin:")
         socket.socket_info = JSON.parse(json)
-        console.log(socket.socket_info)
-        connecions.push(socket)
+        let da_agme = games[socket.socket_info.hostname]
+        if(da_agme == undefined){
+            console.log("no correct host found")
+            socket.disconnect()
+            return 0
+        }
+        da_agme.acceptConnection(socket)
     })
 
     socket.on("disconnect", () => {
-        const index = connecions.indexOf(socket);
-        // only splice array when item is found
-        console.log("disconnected", socket.socket_info)
-        if (index > -1) { 
-            connecions.splice(index, 1); // 1 is for only 1
+        let da_agme = games[socket.socket_info.hostname]
+        if(da_agme != undefined){
+            da_agme.disconnectSocket(socket)
         }
-        socket.disconnect()
     })
 }
+
 
 let connecions = []
 
 const PORT = 8080
-server.use(bParse.urlencoded({extended: true}))
+server.use(express.urlencoded({extended: true}))
+server.use(express.json({extended: true}))
 server.use((req, res, next) => {
     //console.log(req.body)
     //console.log(req.query)
     next()
 })
 
-
-let server_socket;
-
 startHttpsServer(https, server, PORT)
 
-server_socket.of("/socket").on("connection", newSocketHandle)
+server_socket.on("connection", newJoin)
+
 
 function startHttpsServer(https, app, PORT){
     //create and listen to https server
@@ -75,59 +152,32 @@ function startHttpsServer(https, app, PORT){
 server.get("/socket.io/socket.io.js", (req, res, next) => {
     res.sendFile(j(__dirname, "node_modules/socket.io-client/dist/socket.io.js"))
 })
-
 server.get("/assets/*", (req, res, next) => {
     res.sendFile(j(__dirname, req.path))
 })
-
-
 server.get("/online/join", (req, res, next) => {
     res.sendFile(j(__dirname, "join.html"))
 
 })
-
 server.get("/online/host", (req, res, next) => {
     //create a new game
     res.sendFile(j(__dirname, "host.html"))
 })
-
-
-server.post("/online/host", (req, res, next) => {
-    //create a new game
-
-})
-
 server.get("*", (req, res, next) => {
     res.redirect("/online/join")
+})
+
+server.post("/online/host", (req, res, next) => {
+    console.log(req.body)
+    //create a new game
+    if( req.body.hostname == undefined){next()}
+    if(games[req.body.hostname] != undefined){/*game exists*/ next()}
+
+    games[req.body.hostname] = new newGame(req, server_socket)
+    res.send(JSON.stringify({path: j("/socket/join/", req.body.hostname)}))
 })
 
 
 server.post("*", (req, res, next) => {
     res.send("We dont do that here")
 })
-
-
-function getChar() {
-    return new Promise((res, rej) => {
-        let buffer = Buffer.alloc(1)
-        fs.read(0, buffer, 0, 1, undefined, (err, bytesRead, buffer2) => {
-            res(buffer2.toString('utf8'))
-        })
-    })
-}
-
-function getCharBlock(){
-    let buffer = Buffer.alloc(1)
-    fs.readSync(0, buffer, 0, 1)
-    return buffer.toString('utf8')
-}
-
-/*
-while (true){
-    console.log("press s")
-    let char = getCharBlock()
-    if(char == "s"){
-        connecions.forEach((el) => {console.log(el.socket_info)})
-    }
-}
-*/
